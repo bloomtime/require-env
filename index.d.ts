c98a945a05672625c06569c548655dc2646f14b8
Jip Stavenuiter <jipstavenuiter@gmail.com>
2017-08-01 12:42:02 +0200

    Use correct NodeUrl

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 // Type definitions for require-env 0.1.0
 // Project: require-env
 // Definitions by: Jip Stavenuiter https://github.com/Jipperism
+import * as NodeUrl from 'url';
 
 export function contains(name: string): boolean;
 export function inherit(filename: string): void;
 export function require(name: string): string;
-export function requireUrl(name: string): URL;
+export function requireUrl(name: string): NodeUrl.Url;
